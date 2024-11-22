dcbb02f7ff9e04cc966896c3d49c31712912d160
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-22 10:47:57 +0100

    klar

---
+++ Näsan steg 3 övning 6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class program
+{
+    
+static void Main(string[] args)
+{
+string tal;
+int tal2;
+bool det = false;
+
+
+    while(det == false)
+{
+    Console.Clear();
+    Console.WriteLine("Vänligen skriv in ett tal");
+    tal = Console.ReadLine();
+    int.TryParse(tal, out tal2);
+
+    if(int.TryParse(tal, out tal2))
+    {
+        Console.Clear();
+        Console.WriteLine("Du skrev " + GetNumberInput(tal2) + ", tryck på enter om du vill fortsätta");
+        Console.ReadLine();
+    }else
+    {
+        Console.Clear();
+        continue;
+    }
+}
+
+
+}
+static int GetNumberInput(int blabla)
+{
+    return blabla;
+}
+
+}
